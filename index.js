bf03587ffcc501032ef9eefe7f1e9df9cc391a2c
JIAWEN <leejiawen98@gmail.com>
2022-07-13 02:23:04 +0800

    Added retrieval and insertion of team information into database

--- index.js
+++ index.js
@@ -18,3 +18,34 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.listen(3001, () => {
     console.log('running on port 3001');
 })
+
+// Routes
+app.get('/api/getAllTeamInformation', (req, res) => {
+    const sqlSelect = "SELECT * FROM team_information";
+    db.query(sqlSelect, (err, result) => {
+        if (err) {
+            res.status(400).send(err.sqlMessage);
+        } else {
+            res.send(result)
+        }
+    });
+});
+
+app.post('/api/insertTeamInformation', (req, res) => {
+    const teamList = req.body.teamList;
+
+    db.query("DELETE FROM team_information", (err, result) => {
+        if (result) {
+            const sql = "INSERT INTO team_information (team_name, group_number, registration_date) VALUES ?";
+            const values = teamList.map(team => [team.team_name, team.group_number, team.registration_date])
+            db.query(sql, [values], (err, result) => {
+                if (result) {
+                    res.status(200).send("OK");
+                }
+                if (err) {
+                    res.status(400).send(err.sqlMessage);
+                }
+            });
+        }
+    });
+});
